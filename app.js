b3968cee8811e438071b6c60b015a583289968fe
Tyson Nguyen <yngu4822@gmail.com>
2023-03-17 12:31:58 +1100

    implemeneted entire functionality for webpage buttons

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 //Safety practice of letting HTML and CSS load first before JS
 document.addEventListener("DOMContentLoaded", () => {CreateCells(16)});
 
+//global var for color used in bg-color change of the whiteboard
+let color = "black";
+
 //1.Function to make grid cells within the board element
 function CreateCells(inputNum)
 {
     //style the element into a grid structure
     let whiteBoard = document.querySelector('.whiteboard');
+    let cells = whiteBoard.querySelectorAll('.cell');
+    cells.forEach(cell => cell.remove());
     whiteBoard.style.gridTemplateColumns = `repeat(${inputNum},1fr)`;
     whiteBoard.style.gridTemplateRows = `repeat(${inputNum},1fr)`;
     //fill up the grid with divs representing cells
@@ -13,27 +18,41 @@ function CreateCells(inputNum)
     for (let i = 0; i < maxCells; i++)
     {
         let cell = document.createElement('div');
+        cell.setAttribute("class", "cell");
         cell.style.border = "0.5px solid black";
         cell.style.backgroundColor = "white";
         whiteBoard.insertAdjacentElement("beforeend", cell);
-        cell.addEventListener("mouseover", () => {
-            cell.style.backgroundColor = "black";
-        })
+        cell.addEventListener("mouseover", ColorDiv)
     }
 }
 
 //2. onclick event to trigger new grid according to inputSize
+
 //object assigned to button
 let buttonclick = document.querySelector('.update-btn');
+
 //trigger functions when click is performed on the button 
 buttonclick.addEventListener("click", () => {
     let size = InputSize();
-    CreateCells(size);
+    if(!isNaN(size)|| size != null)
+    {
+        CreateCells(size);
+
+    }
+    else
+    {
+        CreateCells(16);
+    }
 })
 
+//3. DOM reference the remaining buttons of the UI to their respective functions
+//RESET button- sets all div cells bg-color back to white 
+let buttonReset = document.querySelector('.reset-btn');
+buttonReset.addEventListener("click", () => {
+    ResetGrid();
+});
 
-
-//function to get inputSize 
+//function to get value of user input which is used in CreateCell(),determines max cell no.
 function InputSize() {
     //1.Instainiate object for input text
     let input = parseInt(document.querySelector('.text-box').value);
@@ -41,10 +60,12 @@ function InputSize() {
     if (input === '' ||isNaN(input))
     {
         alert("Please put a number");
+        CreateCells(16);
     }
     else if (input <= 0)
     {
         alert("Please put in a number greater than 1");
+        CreateCells(16);
     }
     else
     {
@@ -53,9 +74,25 @@ function InputSize() {
     }
 }
 
+//function to reset bg-color of all cells in whiteboard back to white
+function ResetGrid(){
+    let divList = document.querySelectorAll('.cell');
+    divList.forEach(div => div.style.backgroundColor = 'white');
+}
 
+//changes bg-color o
+function ColorDiv(){
+    if (color == 'random')
+    {
+        this.style.backgroundColor = `hsl(${Math.random() * 360} ,100% , 50% )`;
+    }
+    else
+    {
+        this.style.backgroundColor = "black";
+    }
+}
 
-//Creating a box that houses the grid
-// const box = document.createElement('div');
-// box.className = 'whiteboard';
-// document.body.appendChild(box);
+//changes color var to value according to button triggered (black or random color)
+function ColorChoice(selectedColor){
+    color = selectedColor;
+}
